fa9d133efb95d62b99306eb285817c6d5c99a354
nettours <tecabdou@live.com>
2026-04-25 01:32:00 +0200

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -71,7 +71,7 @@ export default function Footer() {
       <div className="footer-bottom">
         <div className="container">
           <p>© {year} بلدية سيدي علي. جميع الحقوق محفوظة.</p>
-          <p>Réalisé par <a href="https://snetpeodz.com" target="_blank">SnetProDz.com</a></p>
+          <p>Réalisé par <a href="https://snetpeodz.com" target="_blank" rel="noreferrer">SnetProDz.com</a></p>
         </div>
       </div>
     </footer>
